18868581dcf59d965955877451240ab83296d1c3
kokatsuna <kokatsuna@yandex.ru>
2017-04-08 13:49:02 +0300

    Add fix

--- vendors.js
+++ vendors.js
@@ -1,49 +1,41 @@
-require('babel-polyfill')
-require('promise-polyfill')
+require('axios')
+require('bootstrap')
+require('google-map-react')
+require('jwt-decode')
+require('moment')
+require('moment-timezone')
 require('outdated-browser-pro')
-
-require('autoprefixer
-require('axios
-require('bootstrap
-require('font-awesome
-require('google-map-react
-require('jwt-decode
-require('material-ui
-require('moment
-require('moment-timezone
-require('outdated-browser-pro
-require('promise-polyfill
-require('rc-collapse
-require('react
-require('react-accordion-components
-require('react-addons-css-transition-group": "^15.4.0",
-require('react-addons-transition-group": "^15.0.0",
-require('react-bootstrap": "^0.30.6",
-require('react-data-components": "^1.1.0",
-require('react-data-grid": "^2.0.1",
-require('react-dom": "^15.4.0",
-require('react-grid-layout": "^0.14.4",
-require('react-moment": "0.2.2",
-require('react-motion": "^0.4.5",
-require('react-paginate": "^4.3.1",
-require('react-promise": "^1.1.2",
-require('react-redux": "^5.0.3",
-require('react-redux-i18n": "^1.8.0",
-require('react-redux-toastr": "^5.0.0",
-require('react-router": "^3.0.0",
-require('react-router-active-component": "^4.0.0",
-require('react-router-bootstrap": "^0.23.1",
-require('react-router-redux": "^4.0.7",
-require('react-scrollbar": "^0.5.1",
-require('react-select": "^1.0.0-rc.2",
-require('react-sidebar": "^2.2.1",
-require('react-tap-event-plugin": "^2.0.1",
-require('react-transform-catch-errors": "^1.0.2",
-require('react-transform-hmr": "^1.0.4",
-require('react-validate": "^0.1.3",
-require('redux": "^3.6.0",
-require('redux-form": "^6.2.0",
-require('redux-logger": "^3.0.1",
-require('redux-thunk": "^2.1.0",
-require('screenfull": "^3.1.0",
-require('validator": "^7.0.0",
+require('promise-polyfill')
+require('rc-collapse')
+require('react')
+require('react-accordion-components')
+require('react-addons-css-transition-group')
+require('react-addons-transition-group')
+require('react-bootstrap')
+require('react-data-components')
+require('react-data-grid')
+require('react-dom')
+require('react-grid-layout')
+require('react-moment')
+require('react-motion')
+require('react-promise')
+require('react-redux')
+require('react-redux-i18n')
+require('react-redux-toastr')
+require('react-router')
+require('react-router-active-component')
+require('react-router-bootstrap')
+require('react-router-redux')
+require('react-scrollbar')
+require('react-select')
+require('react-sidebar')
+require('react-tap-event-plugin')
+require('react-transform-catch-errors')
+require('react-transform-hmr')
+require('react-validate')
+require('redux')
+require('redux-form')
+require('redux-logger')
+require('redux-thunk')
+require('screenfull')
+require('validator')
